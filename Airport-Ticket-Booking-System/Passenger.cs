07f7647b1aa2b0f34266f5e4ec06f7912b989eef
Francisco Watson <francisco@watson.com.ar>
2024-02-08 16:51:31 -0300

    Add Passenger Model

---
+++ Airport-Ticket-Booking-System/Passenger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking_System
+{
+    public class Passenger
+    {
+        public int PassengerId { get; set; }
+        public int Name { get; set; }
+
+        public Passenger(int passengerId, int name)
+        {
+            PassengerId = passengerId;
+            Name = name;
+        }
+
+    }
+}
