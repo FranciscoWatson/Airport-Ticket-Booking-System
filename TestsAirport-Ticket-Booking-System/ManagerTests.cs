487f91524a3c79bc6f4108a8a40057d678d9bed1
Francisco Watson <francisco@watson.com.ar>
2024-02-09 10:51:20 -0300

    Add xUnit Tests

---
+++ TestsAirport-Ticket-Booking-System/ManagerTests.cs
@@ -0,0 +1,11 @@
+using Airport_Ticket_Booking_System;
+namespace TestsAirport_Ticket_Booking_System
+{
+    public class ManagerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
