62b97b6f54d90f346bf3bf87a506683388a1783a
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-02-10 00:59:28 -0300

    Add FileManager to import Flights from Csv

---
+++ Airport-Ticket-Booking-System/Exceptions/ErrorImportingCsv.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking_System.Exceptions
+{
+    public class ErrorImportingCsvException: Exception
+    {
+        public ErrorImportingCsvException(string message) : base(message) { }
+    }
+}
