5d326820ab39132f95b27b1114df8d61ed7cbf06
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-02-12 17:25:04 -0300

    Add Test for importing Flights from CSV file

---
+++ Test-Airport-Ticket-Booking-System/FileSystemTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Airport_Ticket_Booking_System;
+using Airport_Ticket_Booking_System.Enums;
+namespace Test_Airport_Ticket_Booking_System
+{
+    public class FileSystemTests
+    {
+        [Fact]
+        public void ImportFlightsFromCsvTest()
+        {
+            FileSystem fileSystem = new FileSystem(@"C:\Users\Francisco\Desktop\test\test.csv");
+
+            Assert.True(fileSystem.flights.Count > 0);
+        }
+    }
+}   
